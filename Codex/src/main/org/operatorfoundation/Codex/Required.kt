e74ae4190476a6964b72e14d345e953f51d2417f
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-08-25 14:17:31 -0500

    Initial import

---
+++ Codex/src/main/org/operatorfoundation/Codex/Required.kt
@@ -0,0 +1,18 @@
+//class Required:
+//    def __init__(self, r):
+//self.r = r
+//
+//def __len__(self):
+//return 1
+//
+//def __str__(self):
+//return 'Required({r})'.format(r=self.r)
+//
+//def encode(self, n):
+//return self.r
+//
+//def decode(self, n):
+//if n != self.r:
+//raise ValueError('Required({r}) != {n}'.format(r=self.r, n=n))
+//
+//return 0
